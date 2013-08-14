26ff3ada2ff90df4bce8717721cf2045892244a7
Shuntaro Edagawa <team.ganeme@gmail.com>
2013-08-14 21:52:12 +0900

    Todo Add fix

--- scripts/app.js
+++ scripts/app.js
@@ -75,6 +75,30 @@ $(function () {
 			timeout: 10000
 		}).done(function (data) {
 			//通信の成功時
+			if (typeof data == 'object') {
+				var dataBody = data.body,
+						dataCreated = data.created,
+						dataId = data.id,
+						dataName = data.app_name;
+				//TODOデータの取得
+				var js_todo_item = '<li class="js_todo_item bx_todo_item"></li>',
+						js_todo_form = '<form class="js_todo_form"></form>',
+						js_tx_todo = '<label class="js_tx_todo bx_tx_todo">' + dataBody + '</label>',
+						js_todo_edit = '<input type="text" class="js_todo_edit bx_input_edit" name="body" value="' + dataBody + '">',
+						js_todo_id = '<input type="hidden" class="js_todo_id" name="id" value="' + dataId + '">',
+						js_todo_name = '<input type="hidden" class="js_todo_name" name="app_name" value="' + dataName + '">',
+						js_btn_up = '<button class="btn js_btn_up">上へ移動</button>',
+						js_btn_down = '<button class="btn js_btn_down">下へ移動</button>',
+						js_btn_edit = '<button class="btn js_btn_edit">編集</button>',
+						js_btn_complete = '<button class="btn js_btn_complete">完了</button>',
+						$js_btn_rewrite = $('<button class="btn js_btn_rewrite bx_btn_rewrite">修正</button>'),
+						js_add_time = '<p class="tx_add_time js_add_time">' + dataCreated + '</p>',
+						todoForm = $(js_todo_form).append(js_tx_todo).append(js_todo_edit).append(js_todo_id).append(js_todo_name),
+						todoElm = $(js_todo_item).append(todoForm).append(js_btn_up).append(js_btn_down).append(js_btn_edit).append($js_btn_rewrite).append(js_btn_complete).append(js_add_time),
+						$todoElm = (todoElm);
+				//TODOをリストに追加する
+				$todoElm.prependTo(js_todo_content);
+			}
 			for (var i = 0, max = data.length; i < max; i++) {
 				//TODOデータの取得
 				var dataBody = data[i].body,
@@ -97,40 +121,10 @@ $(function () {
 						todoElm = $(js_todo_item).append(todoForm).append(js_btn_up).append(js_btn_down).append(js_btn_edit).append($js_btn_rewrite).append(js_btn_complete).append(js_add_time),
 						$todoElm = (todoElm);
 				//TODOをリストに追加する
-				$todoElm.prependTo(js_todo_content);
+				$todoElm.appendTo(js_todo_content);
 			}
-
 			//リストを囲むul要素を表示する
 			$(js_todo_content).show();
-
-			//Completeボタンをクリック
-			$($('.js_btn_complete')).on('click', function () {
-				var completeId = $(this).parent().find('.js_todo_id').attr('value'),
-						completeName = $(this).parent().find('.js_todo_name').attr('value');
-				//Ajax通信の設定
-				$.ajax({
-					url: 'http://cshooljs.dynalogue.com/api/memo/' + completeId + '/?app_name=' + completeName,
-					type: 'DELETE',
-					//data: $(this).parent().find('.js_todo_form').serialize(),
-					timeout: 10000
-				}).done(function () {
-console.log($todoItem);
-debugger;
-					//通信の成功時
-					//テキストを「Complete!」として表示を消す
-					$(this).parent().css({'padding': '3em 0 3em 12px', 'fontWeight': 'bold'}).html('Complete!').fadeOut('slow', function () {
-						$(this).remove();
-
-						//Todoリストが０になった場合、ul要素を非表示にする
-						if ($('.js_todo_item').length === 0) {
-							$('#js_todo_content').hide();
-						}
-					});
-				}).fail(function () {
-					//通信の失敗時
-					displayErrorText();
-				});
-			});
 
 			//ボタンを上に移動する
 			//TASK ここを関数化したい && 一番上か下の場合はボタンを無効化 && バグってる
@@ -186,6 +180,42 @@ debugger;
 						var text = data.body;
 						$todoItem.find('.js_tx_todo').html(text);
 						$todoItem.removeClass('is_editing');
+					}).fail(function () {
+						//通信の失敗時
+						displayErrorText();
+					});
+				});
+			}
+
+			//TODOを完了する
+			$('.js_btn_complete').on('click', function () {
+				todoComplete($(this));
+			});
+			//TODOを完了するための関数
+			function todoComplete(elm) {
+				var $todoItem = elm.parent('.js_todo_item');
+
+				//Completeボタンをクリック
+				$todoItem.find('.js_btn_complete').on('click', function () {
+					var completeId = $(this).parent().find('.js_todo_id').attr('value'),
+							completeName = $(this).parent().find('.js_todo_name').attr('value');
+					//Ajax通信の設定
+					$.ajax({
+						url: 'http://cshooljs.dynalogue.com/api/memo/' + completeId + '/?app_name=' + completeName,
+						type: 'DELETE',
+						//data: $(this).parent().find('.js_todo_form').serialize(),
+						timeout: 10000
+					}).done(function () {
+						//通信の成功時
+						//テキストを「Complete!」として表示を消す
+						$todoItem.css({'padding': '3em 0 3em 12px', 'fontWeight': 'bold'}).html('Complete!').fadeOut('slow', function () {
+							$(this).remove();
+
+							//Todoリストが０になった場合、ul要素を非表示にする
+							if ($('.js_todo_item').length === 0) {
+								$('#js_todo_content').hide();
+							}
+						});
 					}).fail(function () {
 						//通信の失敗時
 						displayErrorText();
